73972ae00fc37aebe787738fdb75ebdd4bcefa12
RomDom0731 <RomDPas0731@gmail.com>
2024-07-18 12:55:40 -0400

    Initial Commit

---
+++ To-Do-List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do-List
+//
+//  Created by Roman Pasqualone on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
